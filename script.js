c66b83987d6ead4fb75861393353de73b8cac2ab
shekinah <nyamburamary1212@gmail.com>
2023-09-19 10:17:56 +0300

    styled script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const image = document.querySelector(".image")
+
+image.addEventListener("mouseover", function() {
+  image.style.transform = "scale(1.1)";
+  image.style.transition = "transform 0.3s ease";
+});
+
+image.addEventListener("mouseout", function() {
+  image.style.transform = "scale(1)";
+  image.style.transition = "transform 0.3s ease";
+});
 
